109de330a1276a4c8727a2a40be2ff3fb81e2737
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-04-25 14:19:37 +0200

    feat(nvim): add mason

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,5 +6,6 @@ require "user.autocmd"
 spec "user.telescope"
 spec "user.neogit"
 spec "user.colorscheme"
+spec "user.mason"
 
 require "user.lazy"
---
+++ .config/nvim/lua/user/mason.lua
@@ -0,0 +1,40 @@
+local M = {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+  },
+}
+
+function M.config()
+  local servers = {
+    "lua_ls",
+    "cssls",
+    "html",
+    "ts_ls",
+    "bashls",
+    "jsonls",
+  }
+
+  require("mason").setup {
+    ui = {
+      border = "rounded",
+    },
+  }
+
+  require("mason-lspconfig").setup {
+    ensure_installed = servers,
+    automatic_installation = true,
+  }
+end
+
+return M
+
+
+
+
+
+
+
+
+
+
