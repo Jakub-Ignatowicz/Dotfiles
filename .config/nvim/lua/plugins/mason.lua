d33e6308b350e908e2fcf6e76e397729cebf6603
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-19 14:28:02 +0200

    refactor(nvim)

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,37 +1,45 @@
 local M = {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-  },
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+	},
 }
 
 function M.config()
-  local servers = {
-    "lua_ls",
-    "cssls",
-    "html",
-    "ts_ls",
-    "bashls",
-    "jsonls",
-  }
+	local servers = {
+		"lua_ls",
+		"cssls",
+		"html",
+		"ts_ls",
+		"bashls",
+		"jsonls",
+	}
 
-  require("mason").setup {
-    ui = {
-      border = "rounded",
-    },
-  }
+	require("mason").setup({
+		ui = {
+			border = "rounded",
+		},
+	})
 
-  require("mason-lspconfig").setup {
-    ensure_installed = servers,
-    automatic_installation = true,
-  }
+	require("mason-lspconfig").setup({
+		ensure_installed = servers,
+		automatic_installation = true,
+	})
 
+	require("mason-lspconfig").setup_handlers({
+		function(server_name)
+			require("lspconfig")[server_name].setup({})
+		end,
 
-  require("mason-lspconfig").setup_handlers {
-    function(server_name)
-      require("lspconfig")[server_name].setup {}
-    end,
-  }
+		["omnisharp"] = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.omnisharp.setup({
+				cmd = {
+					"/Users/admin/.local/share/nvim/mason/bin/omnisharp",
+				},
+			})
+		end,
+	})
 end
 
 return M
