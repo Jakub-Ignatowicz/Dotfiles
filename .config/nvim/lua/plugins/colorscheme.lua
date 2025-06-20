545f1a7274dccd65dccf12e746c09d5c1e59b3d0
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-21 00:12:20 +0200

    style(nvim): make nvim transparent

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,17 @@
 local M = {
   "LunarVim/darkplus.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  lazy = false,   -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  vim.cmd([[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalNC guibg=NONE ctermbg=NONE
+  hi VertSplit guibg=NONE ctermbg=NONE
+  hi StatusLine guibg=NONE ctermbg=NONE
+  hi LineNr guibg=NONE ctermbg=NONE
+]])
 end
 
 return M
