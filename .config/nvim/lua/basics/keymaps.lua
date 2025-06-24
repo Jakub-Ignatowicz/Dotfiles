26503f8d4549de6c3044f2307f26d68843584a42
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-24 13:45:56 +0200

    style(nvim): change localleader bind

--- .config/nvim/lua/basics/keymaps.lua
+++ .config/nvim/lua/basics/keymaps.lua
@@ -3,7 +3,9 @@ opts = { noremap = true, silent = true }
 
 keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = ";"
+
+keymap("n", "<C-i>", "<C-i>", opts)
 
 keymap("i", "jk", "<Esc>", opts)
 
